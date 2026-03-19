f837f04c7c566f114c4b978b541525ba91835ab2
Alok Verma <aadiix23@gmail.com>
2026-03-19 14:16:22 +0530

    Update Path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const connectDB = require("./config/db");
-const userRoute = require("./routes/userRoute");
+const userRoute = require("./modules/auth/routes/userRoute");
 
 app.use(express.json());
 app.use("/api/users", userRoute);
